c522b5422eec631bb34501b74b22106d88dab2a8
kota.araki <ec2-user@ip-172-31-5-76.us-east-2.compute.internal>
2018-06-19 06:22:54 +0000

    want

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -13,12 +13,33 @@
                             @else
                                 <p class="item-title">{{ $item->name }}</p>
                             @endif
-                            <div class="buttons text-center">
+                            <td>
+                            <span class="form-inline　text-center">
+                            <span class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
                                 @endif
-                            </div>
+                            </span>
+                            <span class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
+                            </span>
+                            </span>
+                            </td>
                         </div>
+                        
+                        @if (isset($item->want_count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->want_count}} Wants</p>
+                            </div>
+                        @endif
+                        
+                        @if (isset($item->have_count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->have_count}} Has</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
