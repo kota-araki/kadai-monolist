916425ab2c4b47f8819e484cf0fd50c10e765c8c
kota.araki <ec2-user@ip-172-31-5-76.us-east-2.compute.internal>
2018-06-20 01:19:35 +0000

    ranking

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class RankingController extends Controller
+{
+    public function want()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as want_count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('want_count', 'DESC')->take(10)->get();
+
+        return view('ranking.want', [
+            'items' => $items,
+        ]);
+    }
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as have_count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('have_count', 'DESC')->take(10)->get();
+
+        return view('ranking.have', [
+            'items' => $items,
+        ]);
+    }
+}
